f37fd9c6cc95f088c186c03153070ff8cafcd2f6
emirkovacevic <emir.kovacevic@lynkz.com.au>
2023-09-03 15:41:22 +1000

    gsgsdfgsd

--- HttpTrigger/index.js
+++ HttpTrigger/index.js
@@ -1,8 +1,12 @@
 module.exports = async function (context, req) {
   context.log("JavaScript HTTP trigger function processed a request.");
-  
+
+  // Set the Content-Type header to indicate JSON response
   context.res = {
     status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
     body: {
       azurefunction: "returning some dumb data from azure function",
     },
